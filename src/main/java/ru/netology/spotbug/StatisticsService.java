32aa02fbac8ce3a9475df6bbaea2da24ee04f85e
Suntcova Daria <batareyka23@gmail.com>
2022-07-20 15:10:49 +0300

    v1.0

--- src/main/java/ru/netology/spotbug/StatisticsService.java
+++ src/main/java/ru/netology/spotbug/StatisticsService.java
@@ -1,4 +1,4 @@
-package ru.netology.statistic;
+package ru.netology.spotbug;
 
 public class StatisticsService {
     /**
--- src/test/java/ru/netology/spotbug/StatisticsServiceTest.java
+++ src/test/java/ru/netology/spotbug/StatisticsServiceTest.java
@@ -1,4 +1,4 @@
-package ru.netology.statistic;
+package ru.netology.spotbug;
 
 import org.junit.jupiter.api.Test;
 
